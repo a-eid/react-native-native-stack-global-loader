475fb7d95003606551c29bacae547ec7c35a980f
Ahmed Eid <a.eid@yandex.com>
2020-12-04 13:05:11 +0200

    ...

--- App.tsx
+++ App.tsx
@@ -1,35 +1,58 @@
-import 'react-native-gesture-handler';
-import {enableScreens} from 'react-native-screens';
 import * as React from 'react';
+import {enableScreens} from 'react-native-screens';
+enableScreens();
+
+import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from 'react-native-screens/native-stack';
-
-import {ModalScreen} from './src/modal-screen';
-import {NormalScreen} from './src/normal-screen';
-import {GlobalLoader} from './src/global-loader';
-import {GlobalToast} from './src/global-toast';
-
-enableScreens();
+import Animated, {useSharedValue} from 'react-native-reanimated';
+import {View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+function NormalScreen({navigation}: any) {
   return (
-    <GlobalToast>
-      <GlobalLoader>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="normal-screen" component={NormalScreen} />
-            <Stack.Screen
-              name="modal-screen"
-              component={ModalScreen}
-              options={{
-                stackPresentation: 'formSheet',
-              }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </GlobalLoader>
-    </GlobalToast>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('modal')}
+        style={{width: 200, height: 70, backgroundColor: 'red'}}
+      />
+    </View>
+  );
+}
+
+function ModalScreen({
+  value,
+}: {
+  value: Animated.SharedValue<boolean>;
+}): JSX.Element {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <TouchableOpacity
+        onPress={() => (value.value = !value.value)}
+        style={{width: 200, height: 70, backgroundColor: 'red'}}
+      />
+    </View>
+  );
+}
+
+export default function App() {
+  const value = useSharedValue<boolean>(false);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="normal" component={NormalScreen} />
+        <Stack.Screen
+          options={{
+            stackPresentation: 'formSheet',
+            gestureEnabled: value.value,
+          }}
+          name="modal"
+          getComponent={() => () => <ModalScreen value={value} />}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
